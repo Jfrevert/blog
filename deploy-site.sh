3342fde6ac856788f6ad13b2a714ad79436a721d
Justin Frevert <jgaingringer@gmail.com>
2020-02-06 00:27:23 -0500

    Add back content folder

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,4 +1,5 @@
 cp -a public/. deploy/
 docker build -t package deploy/.
 docker run --env-file=local.env package
-# find deploy/ ! -name Dockerfile ! -name deploy.sh -maxdepth 1 -type f -delete
+find deploy/ ! -name Dockerfile ! -name deploy.sh -maxdepth 1 -type f -delete
+rm -r deploy/*/
